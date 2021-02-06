fc2c69c888afde0cd47994c84f5161bc8cb0e4df
Hai Anh <anhvth.226@gmail.com>
2021-02-06 09:41:18 +0000

    update

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-# units
-
 # Instalation
 
 pip install git+https://github.com/anhvth/avcv.git
@@ -8,6 +6,11 @@ pip install git+https://github.com/anhvth/avcv.git
 ## Examples
 Convert a folder of images to video
 
+Take a look at the function definition by only giving it a function name, ex. images_to_video
+```
+    python -m avcv.run images_to_video
+```
+Now give it the positional arguments
 ```bash
-    python -m avcv.run images-to-video  /data/fisheye_cams/20201207/timecity_T13_1/ ../output.mp4
+    python -m avcv.run images-to-video  -a /data/fisheye_cams/20201207/timecity_T13_1/ ../output.mp4
 ```
--- avcv/run.py
+++ avcv/run.py
@@ -1,16 +1,23 @@
 from avcv.vision import *
-from avcv.vision import *
+from avcv.utils import *
 
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument('task', choices=["images_to_video"])
-    parser.add_argument('argument', nargs="+")
+    parser.add_argument('task')
+    parser.add_argument('--arguments', '-a', nargs="+", default=None)
     args = parser.parse_args()
+
     fn = eval(args.task)
-    print("Running ", fn.__name__)
-    print("Argument", "value")
-    for value, var_name in zip(args.argument, fn.__code__.co_varnames):
-        print(var_name, ":", value)
-    fn(*args.argument)
+    
+    if args.arguments is  None:
+        print(f"------Function  {fn.__name__} deffinition------------------")
+        print_source(fn)
+        print("----------------------------------------------------")
+        
+    else:
+        print("Argument", "value")
+        for value, var_name in zip(args.arguments, fn.__code__.co_varnames):
+            print(var_name, ":", value)
+        fn(*args.arguments)
 
