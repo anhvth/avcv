014be819f56bcff34f46418c46667b840d1c7657
anh vo <anhvth.226@gmail.com>
2021-07-17 23:15:19 +0700

    update

--- avcv/_nbdev.py
+++ avcv/_nbdev.py
@@ -4,6 +4,9 @@ __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
 index = {"plot_images": "00_visualize.ipynb",
          "multi_thread": "01_process.ipynb",
+         "get_name": "03_utils.ipynb",
+         "download_file_from_google_drive": "03_utils.ipynb",
+         "mkdir": "03_utils.ipynb",
          "put_text": "03_utils.ipynb",
          "images_to_video": "03_utils.ipynb",
          "get_paths": "03_utils.ipynb"}
