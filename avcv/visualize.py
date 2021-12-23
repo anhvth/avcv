84dda4b677394954afe90444e6a72d5f56c834b6
anh vo <anhvth.226@gmail.com>
2021-12-23 20:08:16 +0000

    update

--- avcv/visualize.py
+++ avcv/visualize.py
@@ -196,19 +196,19 @@ def bbox_visualize(img, boxes, scores, cls_ids, conf=0.5, class_names=None, text
         y0 = int(box[1])
         x1 = int(box[2])
         y1 = int(box[3])
-
-        color = (_COLORS[cls_id] * 255).astype(np.uint8).tolist()
+        color_id = cls_id % len(_COLORS)
+        color = (_COLORS[color_id] * 255).astype(np.uint8).tolist()
         if texts is None:
             text = '{}:{:.1f}%'.format(class_names[cls_id], score * 100)
         else:
             text = texts[i]
-        txt_color = (0, 0, 0) if np.mean(_COLORS[cls_id]) > 0.5 else (255, 255, 255)
+        txt_color = (0, 0, 0) if np.mean(_COLORS[color_id]) > 0.5 else (255, 255, 255)
         font = cv2.FONT_HERSHEY_SIMPLEX
 
         txt_size = cv2.getTextSize(text, font, 0.4, 1)[0]
         cv2.rectangle(img, (x0, y0), (x1, y1), color, 2)
 
-        txt_bk_color = (_COLORS[cls_id] * 255 * 0.7).astype(np.uint8).tolist()
+        txt_bk_color = (_COLORS[color_id] * 255 * 0.7).astype(np.uint8).tolist()
         cv2.rectangle(
             img,
             (x0, y0 + 1),
