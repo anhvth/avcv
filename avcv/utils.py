1ceae87a0f2a5e3baf2f3bf380036abe2f4938a5
anh vo <anhvth.226@gmail.com>
2021-12-27 13:30:48 +0000

    update

--- avcv/utils.py
+++ avcv/utils.py
@@ -318,17 +318,29 @@ def memoize(func):
     return memoized_func
 
 # Cell
-import types
-import uuid
-import copy
-import inspect
-import io
-import pickle
+# Copyright (c) 2021, NVIDIA CORPORATION & AFFILIATES.  All rights reserved.
+#
+# NVIDIA CORPORATION and its licensors retain all intellectual property
+# and proprietary rights in and to this software, related documentation
+# and any modifications thereto.  Any use, reproduction, disclosure or
+# distribution of this software and related documentation without an express
+# license agreement from NVIDIA CORPORATION is strictly prohibited.
+
+"""Facilities for pickling Python code alongside other data.
+
+The pickled code is automatically imported into a separate Python module
+during unpickling. This way, any previously exported pickles will remain
+usable even if the original code is no longer available, or if the current
+version of the code is not consistent with what was originally pickled."""
+
 import sys
+import pickle
+import io
+import inspect
+import copy
+import uuid
+import types
 from typing import Any
-# from classification_models.all_package import *
-
-
 class EasyDict(dict):
     """Convenience class that behaves like a dict but allows access with the attribute syntax."""
 
@@ -345,33 +357,15 @@ class EasyDict(dict):
         del self[name]
 
 
+#----------------------------------------------------------------------------
 
-# Copyright (c) 2021, NVIDIA CORPORATION.  All rights reserved.
-#
-# NVIDIA CORPORATION and its licensors retain all intellectual property
-# and proprietary rights in and to this software, related documentation
-# and any modifications thereto.  Any use, reproduction, disclosure or
-# distribution of this software and related documentation without an express
-# license agreement from NVIDIA CORPORATION is strictly prohibited.
-
-"""Facilities for pickling Python code alongside other data.
-
-The pickled code is automatically imported into a separate Python module
-during unpickling. This way, any previously exported pickles will remain
-usable even if the original code is no longer available, or if the current
-version of the code is not consistent with what was originally pickled."""
-
-
-# ----------------------------------------------------------------------------
-
-_version = 6         # internal version number
-_decorators = set()     # {decorator_class, ...}
-_import_hooks = []        # [hook_function, ...]
+_version            = 6         # internal version number
+_decorators         = set()     # {decorator_class, ...}
+_import_hooks       = []        # [hook_function, ...]
 _module_to_src_dict = dict()    # {module: src, ...}
 _src_to_module_dict = dict()    # {src: module, ...}
 
-# ----------------------------------------------------------------------------
-
+#----------------------------------------------------------------------------
 
 def persistent_class(orig_class):
     r"""Class decorator that extends a given class to save its source code
@@ -454,25 +448,23 @@ def persistent_class(orig_class):
 
         @property
         def init_kwargs(self):
-            return EasyDict(copy.deepcopy(self._init_kwargs))
+            return dnnlib.EasyDict(copy.deepcopy(self._init_kwargs))
 
         def __reduce__(self):
             fields = list(super().__reduce__())
             fields += [None] * max(3 - len(fields), 0)
             if fields[0] is not _reconstruct_persistent_obj:
-                meta = dict(type='class', version=_version, module_src=self._orig_module_src,
-                            class_name=self._orig_class_name, state=fields[2])
-                fields[0] = _reconstruct_persistent_obj  # reconstruct func
-                fields[1] = (meta,)  # reconstruct args
-                fields[2] = None  # state dict
+                meta = dict(type='class', version=_version, module_src=self._orig_module_src, class_name=self._orig_class_name, state=fields[2])
+                fields[0] = _reconstruct_persistent_obj # reconstruct func
+                fields[1] = (meta,) # reconstruct args
+                fields[2] = None # state dict
             return tuple(fields)
 
     Decorator.__name__ = orig_class.__name__
     _decorators.add(Decorator)
     return Decorator
 
-# ----------------------------------------------------------------------------
-
+#----------------------------------------------------------------------------
 
 def is_persistent(obj):
     r"""Test whether the given object or class is persistent, i.e.,
@@ -483,10 +475,9 @@ def is_persistent(obj):
             return True
     except TypeError:
         pass
-    return type(obj) in _decorators  # pylint: disable=unidiomatic-typecheck
+    return type(obj) in _decorators # pylint: disable=unidiomatic-typecheck
 
-# ----------------------------------------------------------------------------
-
+#----------------------------------------------------------------------------
 
 def import_hook(hook):
     r"""Register an import hook that is called whenever a persistent object
@@ -518,8 +509,7 @@ def import_hook(hook):
     assert callable(hook)
     _import_hooks.append(hook)
 
-# ----------------------------------------------------------------------------
-
+#----------------------------------------------------------------------------
 
 def _reconstruct_persistent_obj(meta):
     r"""Hook that is called internally by the `pickle` module to unpickle
@@ -541,13 +531,12 @@ def _reconstruct_persistent_obj(meta):
 
     setstate = getattr(obj, '__setstate__', None)
     if callable(setstate):
-        setstate(meta.state)  # pylint: disable=not-callable
+        setstate(meta.state) # pylint: disable=not-callable
     else:
         obj.__dict__.update(meta.state)
     return obj
 
-# ----------------------------------------------------------------------------
-
+#----------------------------------------------------------------------------
 
 def _module_to_src(module):
     r"""Query the source code of a given Python module.
@@ -559,7 +548,6 @@ def _module_to_src(module):
         _src_to_module_dict[src] = module
     return src
 
-
 def _src_to_module(src):
     r"""Get or create a Python module for the given source code.
     """
@@ -570,11 +558,10 @@ def _src_to_module(src):
         sys.modules[module_name] = module
         _module_to_src_dict[module] = src
         _src_to_module_dict[src] = module
-        exec(src, module.__dict__)  # pylint: disable=exec-used
+        exec(src, module.__dict__) # pylint: disable=exec-used
     return module
 
-# ----------------------------------------------------------------------------
-
+#----------------------------------------------------------------------------
 
 def _check_pickleable(obj):
     r"""Check that the given object is pickleable, raising an exception if
@@ -587,14 +574,13 @@ def _check_pickleable(obj):
         if isinstance(obj, dict):
             return [[recurse(x), recurse(y)] for x, y in obj.items()]
         if isinstance(obj, (str, int, float, bool, bytes, bytearray)):
-            return None  # Python primitive types are pickleable.
-        if f'{type(obj).__module__}.{type(obj).__name__}' in ['numpy.ndarray', 'torch.Tensor']:
-            return None  # NumPy arrays and PyTorch tensors are pickleable.
+            return None # Python primitive types are pickleable.
+        if f'{type(obj).__module__}.{type(obj).__name__}' in ['numpy.ndarray', 'torch.Tensor', 'torch.nn.parameter.Parameter']:
+            return None # NumPy arrays and PyTorch tensors are pickleable.
         if is_persistent(obj):
-            # Persistent objects are pickleable, by virtue of the constructor check.
-            return None
+            return None # Persistent objects are pickleable, by virtue of the constructor check.
         return obj
     with io.BytesIO() as f:
         pickle.dump(recurse(obj), f)
 
-# ----------------------------------------------------------------------------
+#----------------------------------------------------------------------------
