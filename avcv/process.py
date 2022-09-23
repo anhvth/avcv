345c32439ec2703cb9ddc38ced96eba973c9302c
Võ Trần Hải Anh <v.anhvth8@vinai.io>
2022-09-23 04:36:16 +0000

    update

--- avcv/process.py
+++ avcv/process.py
@@ -6,7 +6,10 @@ __all__ = ['multi_thread', 'multi_process']
 # %% ../nbs/01_process.ipynb 3
 import mmcv
 from loguru import logger
-def multi_thread(fn, array_inputs, max_workers=None, desc="Multi-thread Pipeline", unit="Samples", verbose=True, pbar_iterval=50):
+
+def multi_thread(fn, array_inputs, max_workers=None, 
+                 desc="Multi-thread Pipeline", unit="Samples", 
+                 verbose=True, pbar_iterval=50):
     from concurrent.futures import ThreadPoolExecutor
     from functools import partial
     from glob import glob
@@ -19,13 +22,14 @@ def multi_thread(fn, array_inputs, max_workers=None, desc="Multi-thread Pipeline
 
     array_inputs = [(i, _) for i, _ in enumerate(array_inputs)]
     if verbose:
-        progress_bar = tqdm(total=len(array_inputs))
+        logger.info(desc)
+        progress_bar = mmcv.ProgressBar(len(array_inputs))#tqdm(total=len(array_inputs))
     outputs = {}
     with ThreadPoolExecutor(max_workers=max_workers) as executor:
         for i, result in enumerate(executor.map(_wraper, array_inputs)):
             outputs.update(result)
-            if verbose:# and i%pbar_iterval==0:
-                progress_bar.update()
+            if verbose and i%pbar_iterval==0:
+                progress_bar.update(pbar_iterval)
     if verbose:
         logger.info('multi_thread')
     outputs = list(outputs.values())
@@ -36,7 +40,7 @@ def multi_thread(fn, array_inputs, max_workers=None, desc="Multi-thread Pipeline
 def multi_process(f, inputs, num_workers=10):
     from multiprocessing import Pool
     from tqdm import tqdm
-    with Pool(10) as p:
+    with Pool(num_workers) as p:
         it = p.imap(f, inputs)
         total = len(inputs)
         return list(tqdm(it, total=total))
