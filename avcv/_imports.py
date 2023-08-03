8dade8f8f84937de783f47f8e3c0a3501760b355
anh vo <anhvth.226@gmail.com>
2023-08-03 11:51:45 +0700

    update

--- avcv/_imports.py
+++ avcv/_imports.py
@@ -31,7 +31,7 @@ np = LazyModule('numpy')
 cv2 = LazyModule('cv2')
 matplotlib = LazyModule('matplotlib')
 plt = LazyModule('plt', 'import matplotlib.pyplot as plt')
-coco = LazyModule('coco', 'from pycocotools import coco')
+# coco = LazyModule('coco', 'from pycocotools import coco')
 ipdb = LazyModule('ipdb')
 pd = LazyModule('pandas')
 #import ipdb, cv2, matplotlib.pyplot as plt, mmcv, numpy as np, matplotlib
--- avcv/all.py
+++ avcv/all.py
@@ -2,7 +2,7 @@ from avcv._imports import *
 from avcv.utils import *
 from avcv.process import *
 from avcv.visualize import *
-from avcv.coco import *
+# from avcv.coco import *
 
 # import cv2, matplotlib.pyplot as plt, mmcv, numpy as np, os, os.path as osp
 # from glob import glob
