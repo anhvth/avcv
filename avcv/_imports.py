86633a9f075e10c716717e63c3d94e03dd0ede2e
anh vo <anhvth.226@gmail.com>
2024-07-14 20:31:45 -0700

    update

--- avcv/_imports.py
+++ avcv/_imports.py
@@ -30,7 +30,6 @@ from fastcore.parallel import threaded
 from fastcore.script import Param, call_parse
 from lazy_module.core import LazyModule
 from PIL import Image
-
 # Use lazy imports for performance optimization
 mmcv = LazyModule('mmcv')
 np = LazyModule('numpy')
